f4a8b944bd9fff0300654db0e2ea16e78f77bd10
Nikita <nikitaefron06@gmail.com>
2025-02-13 02:48:00 +0100

    summary (required)

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -8,8 +8,13 @@ document.getElementById("startButton").addEventListener("click", function (e) {
   // Запускаем анимацию
   imageContainer.style.animation = "fly-in-out 4.5s ease-in-out forwards";
 
-  // По окончании анимации открываем новую вкладку
+  // По окончании анимации открываем новый сайт в новой вкладке
   setTimeout(function () {
-    window.open("https://therma-sat.net/", "_blank"); // Открываем страницу в новой вкладке
+    window.open("https://therma-sat.net/", "_blank"); // Переход на новый сайт в новой вкладке
   }, 4500); // Задержка в 4.5 секунды (время длительности анимации)
+
+  // Перезагружаем текущую страницу после 5 секунд
+  setTimeout(function () {
+    location.reload(); // Перезагружаем текущую страницу
+  }, 5000); // 5000 мс — 5 секунд после перехода
 });
